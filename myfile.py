091223f9fb2f6145294624882f2a3a532adf9af6
Akhil Adusumilli <akhiladusumilli100@gmail.com>
2025-01-19 18:32:54 -0500

    Added short program with error

--- myfile.py
+++ myfile.py
@@ -1,2 +1,10 @@
-print("Hello World")
+print("Calculating Factors")
+x = int(input("Input Number: "))
 
+arr = []
+
+for i in range(1, x):
+    if (x % i) = 0:
+        arr.append(i)
+
+print(f"Factors: {arr}")
